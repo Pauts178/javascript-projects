c59271c4b9cca939768ba4649621aebe752ac45f
Pauts178 <katherine.pautler@gmail.com>
2024-01-09 19:58:39 -0600

    completed

--- data-and-variables/chapter-examples/readline-greeting-program.js
+++ data-and-variables/chapter-examples/readline-greeting-program.js
@@ -1,3 +1,7 @@
 const input = require('readline-sync');
 
-let name = input.question("Enter your name: ");
+let name = input.question("Enter your first name: ");
+let otherName = input.question ("Enter your last name: ")
+console.log("First name:" , name)
+console.log("Last name:" , otherName)
+console.log("Last, First:" , otherName + "," , name)
