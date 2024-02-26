0443be6b0eeae6241bd51d00230a69b0736825d5
Pauts178 <katherine.pautler@gmail.com>
2024-02-26 17:24:48 -0600

    abort mission

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,7 +4,26 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
-    
+    button.addEventListener('click', function () {
+        paragraph.innerHTML = 'Houston! We have liftoff!';
+    })
+
+    missionAbort.addEventListener('mouseover', function ( event ) {
+        missionAbort.style.backgroundColor = 'red'
+    })
+
+    missionAbort.addEventListener('mouseout', function ( event ) {
+        missionAbort.style.backgroundColor = ''
+    })
+
+    missionAbort.addEventListener('click', function (event) {
+    //    window.confirm('Are you sure you want to abort the mission?');
+       if (window.confirm('Are you sure you want to abort the mission?') == true) {
+        paragraph.innerHTML = 'Mission Aborted! Space shuttle returning home.'
+       }
+    })
+
+
 }
 
 window.addEventListener("load", init);
